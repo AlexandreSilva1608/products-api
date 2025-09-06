03fc58a3bd78cda0e044d8096d0a7a8efaa7e0e7
alexandre <alexandreaugusto1287@gmail.com>
2025-09-06 19:48:22 -0300

    Adicionando tratativa de CORS policy e preço ao DTO de produtos

--- src/main/java/com/productapi/productapi/dto/ProductDTO.java
+++ src/main/java/com/productapi/productapi/dto/ProductDTO.java
@@ -4,17 +4,21 @@ import com.productapi.productapi.model.Product;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class ProductDTO {
 
     private Long productId;
+    private BigDecimal price;
     private Integer quantity;
     private String name;
 
     public ProductDTO(Product product) {
         this.name = product.getName();
         this.productId = product.getId();
+        this.price = product.getPrice();
         this.quantity = product.getStock();
     }
 }
